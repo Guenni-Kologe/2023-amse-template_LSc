0e59b3a533f077943317fd0beabb48b6b0174cec
guenni-kologe <laurin@praxis-schmid.com>
2023-05-22 19:43:10 +0200

    4nd Try

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -8,21 +8,6 @@ df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/
 
 engine = sa.create_engine(f'sqlite:///{db_path}')
 
-dtypes = {
-    'column_1': sa.Integer(),
-    'column_2': sa.String(),
-    'column_3': sa.String(),
-    'column_4': sa.String(),
-    'column_5': sa.String(),
-    'column_6': sa.String(),
-    'column_7': sa.Float(),
-    'column_8': sa.Float(),
-    'column_9': sa.Integer(),
-    'column_10': sa.Float(),
-    'column_11': sa.String(),
-    'column_12': sa.String(),
-    'geo_punkt': sa.String()
-}
+print(df)
 
-
-df.to_sql("airports", engine, if_exists="replace", index=False, dtype=dtypes)
+df.to_sql("airports", engine, if_exists="replace", index=False,)
