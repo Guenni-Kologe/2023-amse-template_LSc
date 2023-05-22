4c11462823f429090e18af3d2d8f7b001c605b87
guenni-kologe <laurin@praxis-schmid.com>
2023-05-22 19:48:11 +0200

    5th try

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -4,10 +4,10 @@ import sqlalchemy as sa
 
 db_path = 'airports.sqlite'
 
-df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', sep="; ", engine='python')
+df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', delimiter = "; ", engine='python')
 
 engine = sa.create_engine(f'sqlite:///{db_path}')
 
-print(df)
+df.head(0).to_sql("airports", engine, if_exists="replace", index=False)
 
-df.to_sql("airports", engine, if_exists="replace", index=False,)
+df.to_sql("airports", engine, if_exists="replace", index=False)
