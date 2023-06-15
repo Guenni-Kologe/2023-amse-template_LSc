ad3be767f955b35bd8514e186620f6440c0decb1
guenni-kologe <laurin@praxis-schmid.com>
2023-06-15 19:17:14 +0200

    Test 3rd try

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,5 @@
 echo ~~~~~~~ Run down the Pipeline so we have the current state ~~~~~~~~~
-cd ..
-python \\data\\pipeline_amse.py
+python ../data/pipeline_amse.py
 echo ~~~~~~~ Run down finished ~~~~~~~~
 echo ~~~~~~~ Now perform the tests ~~~~~~~~~~
 python pipeline_test.py
