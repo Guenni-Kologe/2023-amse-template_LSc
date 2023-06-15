7c02727eb8cca60858535f88992b26f88f7bcd8b
guenni-kologe <laurin@praxis-schmid.com>
2023-06-15 19:27:44 +0200

    tests look better

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
-echo ~~~~~~~ Run down the Pipeline so we have the current state ~~~~~~~~~
+echo "Step 1 - Run down the Pipeline so we have the current state"
 python ../data/pipeline_amse.py
-echo ~~~~~~~ Run down finished ~~~~~~~~
-echo ~~~~~~~ Now perform the tests ~~~~~~~~~~
+echo "Answer: Run down finished"
+echo "Step 2 - Now perform the tests ~~~~~~~~~~
 python pipeline_test.py
-echo ~~~~~~~ Everything seems fine if there is no error code ~~~~~~~~~~
+echo "Answer: Everything seems fine if there is no error code"
