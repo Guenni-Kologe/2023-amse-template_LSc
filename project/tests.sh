5d1a5c319d0460af59fd2e15c3a8ae95bb2e76cc
guenni-kologe <laurin@praxis-schmid.com>
2023-06-15 19:14:23 +0200

    tests corrected 1st

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
-
 echo ~~~~~~~ Run down the Pipeline so we have the current state ~~~~~~~~~
-python ..\\data\\pipeline_amse.py
+python \\data\\pipeline_amse.py
 echo ~~~~~~~ Run down finished ~~~~~~~~
 echo ~~~~~~~ Now perform the tests ~~~~~~~~~~
 python pipeline_test.py
+echo ~~~~~~~ Everything seems fine if there is no error code ~~~~~~~~~~
