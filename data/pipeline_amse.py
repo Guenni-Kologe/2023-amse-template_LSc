2cb691895da60f7f34e4cba12d67a90a30ab5b0b
LSchmid <laurin@praxis-schmid.com>
2023-06-27 18:08:34 +0200

    Exploration + Database

--- data/pipeline_amse.py
+++ data/pipeline_amse.py
@@ -1,104 +1,65 @@
 import pandas as pd
 import sqlalchemy as sa
-import ssl
 import os
 import matplotlib.pyplot as plt
 
-ssl._create_default_https_context = ssl._create_unverified_context
-
+# Retrieve the both datasets from the internet
 current_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(current_dir, 'data.sqlite')
 engine = sa.create_engine(f'sqlite:///{db_path}')
 
-# Load the vehicle data
 vehicle_data = pd.read_csv('https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0020_00.csv', sep=';', encoding='iso-8859-1', skiprows=5, skipfooter=5, engine='python')
+vehicle_data.columns = ['col1', 'col2', 'county'] + list(vehicle_data.columns[3:]) # Rename the first three columns
+vehicle_data.drop(['col1', 'col2'], axis=1, inplace=True) # Drop col1 and col2
+vehicle_data['county'] = vehicle_data['county'].str.split(',').str[0] # Get rid of the values after the comma (e.g., Kreisfreie Stadt etc.)
 
-# Rename the first three columns
-vehicle_data.columns = ['col1', 'col2', 'county'] + list(vehicle_data.columns[3:])
 
-# Drop col1 and col2
-vehicle_data.drop(['col1', 'col2'], axis=1, inplace=True)
-
-# Adjust the column 'county' of vehicle_data
-vehicle_data['county'] = vehicle_data['county'].str.split(',').str[0]
-
-# Load the county data
 county_data = pd.read_csv('https://downloads.suche-postleitzahl.org/v2/public/plz_einwohner.csv')
 
-# Remove numbers from the 'note' column in county_data
 for nbr in range(0, 10):
-    county_data['note'] = county_data['note'].str.replace(f'{nbr}', '')
+    county_data['note'] = county_data['note'].str.replace(f'{nbr}', '') # Remove the ZIP-Code from the 'note' column in county_data
 
-#Remove the first Space, which stays after the number cleaning
-county_data['note'] = county_data['note'].str.replace(' ', '', n=1)
+county_data['note'] = county_data['note'].str.replace(' ', '', n=1) # Remove the first Space, which stays after the number cleaning
+county_data = county_data.groupby('note', as_index=False)['einwohner'].sum() # Group by 'note' and sum the 'einwohner' values
 
-# Group by 'note' and sum the 'einwohner' values
-county_data = county_data.groupby('note', as_index=False)['einwohner'].sum()
-
-# Drop unnecessary columns from county_data
-#county_data = county_data.iloc[:, 1:]
-
-# Connect a database
+# Save the edited data into the SQLite Database
 engine = sa.create_engine(f'sqlite:///{db_path}')
 
-# Save the data to a SQL databases
-vehicle_data.to_sql("vehicle", engine, if_exists="replace", index=False)
+vehicle_data.to_sql("vehicle", engine, if_exists="replace", index=False) # Save the data to a SQL databases
 county_data.to_sql("county", engine, if_exists="replace", index=False)
 
-# Query the data
+# Query the needed data from the SQLite Database and store it in a DataFrame
 query = sa.text("""
 SELECT c.note AS County, c.einwohner AS Inhabitants, v.Pkw AS "Registered Vehicles", v.Ackerschlepper AS Ackerschlepper
 FROM county AS c
 JOIN vehicle AS v ON c.note = v.county
 """)
 
-# Execute the query and fetch the data
-conn = engine.connect()
+conn = engine.connect() # Execute the query and fetch the data
 result = conn.execute(query)
 data = result.fetchall()
 
-# Create a DataFrame
-df = pd.DataFrame(data, columns=["County", "Inhabitants", "Registered Vehicles", "Ackerschlepper"])
+df = pd.DataFrame(data, columns=["County", "Inhabitants", "Registered Vehicles", "Ackerschlepper"]) # Create the DataFrame
 
 # Perform exploratory analysis
 
-# Filter out non-integer values in "Registered Vehicles" column
-filtered_df_pkw = df[df["Registered Vehicles"].notnull() & (df["Registered Vehicles"] != "-")]
-# Filter out non-integer values in "Ackerschlepper" column
-filtered_df_ackerschlepper = df[df["Ackerschlepper"].notnull() & (df["Ackerschlepper"] != "-")]
+filtered_df_pkw = df[df["Registered Vehicles"].notnull() & (df["Registered Vehicles"] != "-")] # Filter out non-integers and NULLs, we do this in this step as otherwise we would need to store a '0' in our DB 
+filtered_df_ackerschlepper = df[df["Ackerschlepper"].notnull() & (df["Ackerschlepper"] != "-")] # or use dropna() which would drop a column even if it would only lack one value
 
-# Convert max_registered_vehicles and max_inhabitants to integers
-max_registered_vehicles = filtered_df_pkw["Registered Vehicles"].astype(int).max()
+max_registered_vehicles = filtered_df_pkw["Registered Vehicles"].astype(int).max() # Convert max_registered_vehicles and max_inhabitants to integers and get the maximum value
 max_inhabitants = filtered_df_pkw["Inhabitants"].astype(int).max()
 
-#print(filtered_df_pkw["Inhabitants"].astype(int).max())
-#print(" <- max | Inhabitants | min - >")
-#print(filtered_df_pkw["Inhabitants"].astype(int).min())
+num_ticks = 10 # Set the desired number of tick marks
 
-#print(filtered_df_pkw["Registered Vehicles"].astype(int).max())
-#print(" <- max | Pkw | min - >")
-#print(filtered_df_pkw["Registered Vehicles"].astype(int).min())
-
-min_registered_vehicles = filtered_df_pkw["Registered Vehicles"].astype(int).min()
-# Set the desired number of tick marks
-num_ticks = 10
-
-# Define the tick positions for the x-axis
-x_ticks_pkw = list(range(0, max_registered_vehicles + 1, max_registered_vehicles // num_ticks))
+x_ticks_pkw = list(range(0, max_registered_vehicles + 1, max_registered_vehicles // num_ticks)) # Define the tick positions for the x-axis
 x_ticks_ackerschlepper = list(range(0, filtered_df_ackerschlepper["Ackerschlepper"].astype(int).max() + 1, filtered_df_ackerschlepper["Ackerschlepper"].astype(int).max() // num_ticks))
 
-# Calculate the range of values in the "Inhabitants" column
-min_inhabitants = filtered_df_pkw["Inhabitants"].astype(int).min()
-max_inhabitants = filtered_df_pkw["Inhabitants"].astype(int).max()
 
-# Calculate the step size for y-axis ticks
-y_range = max_inhabitants - min_inhabitants
-y_tick_step = y_range // num_ticks
+max_inhabitants = filtered_df_pkw["Inhabitants"].astype(int).max() # Again convert inhabitants into integers and then get the maximum
 
-# Define the tick positions for the y-axis
-y_ticks = list(range(min_inhabitants, max_inhabitants + 1, y_tick_step))
+y_ticks = list(range(0, max_inhabitants + 1, max_inhabitants // num_ticks))
 
-# Plot the counties and their amount of registered vehicles
+# Plot the counties inhabitants and their amount of registered vehicles
 plt.figure(figsize=(18, 6))
 
 # Plot for Pkw
@@ -139,5 +100,6 @@ plt.text(0.95, 0.90, f"Most Tractors per Inhabitant: {highest_ratio_county_acker
 plt.text(0.95, 0.85, f"Lowest Tractors per Inhabitant: {lowest_ratio_county_ackerschlepper} ({lowest_ratio_value_ackerschlepper:.2f})", transform=plt.gca().transAxes, ha='right', va='top')
 filtered_df_ackerschlepper.to_sql("result_ackerschlepper", engine, if_exists="replace", index=False)
 
+# Show the plot
 plt.tight_layout()
 plt.show()
--- project/project-plan.md
+++ project/project-plan.md
@@ -3,44 +3,40 @@
 ## Summary
 
 <!-- Describe your data science project in max. 5 sentences. -->
-This project refers to the connection between cars per inhabitant and the availability of railroad stops (Deutsche Bahn) in the respective counties. The aim is to identify counties that have a sparse rail infrastructure and to identify whether they have a higher number of registered cars. 
+This project refers to the connection between cars per inhabitant and the amount of registered vehicles in the respective counties. The aim is to identify counties that have a relatively low population, but a lot of registered vehicles. With that reached, the aim is to compare different vehicle types (e.g. Cars and Tractors) to see if there is a correlation between the population and the amount of registered vehicle types. The Asumption here is that in hardly populated areas public transportation is not that widespread and therefore more vehicles per inhabitant are registered.
 
 ## Rationale
 
 <!-- Outline the impact of the analysis, e.g. which pains it solves. -->
-The results of the project will help identify counties that substitute private vehicles for their sparsely developed rail networks. The results of this analysis should help to expand the rail network in those counties that have a particularly high ratio of vehicles per inhabitant, in order to achieve that after a successful expansion of the rail network fewer private motor vehicles are needed. This could result in more people using public transport and therefore less pollutants, less noise and fewer accidents due to private vehicles.
+The results of the project will help to identify counties that have alot of private vehicles. This could be an indicator for a lack of public transportation or in more general this could be a subject to examine. The project could help the Government or public transportation companies to identify counties with a vast potential for improvement. This could ultimately result in more people using public transport and therefore less pollutants, less noise and fewer accidents due to private vehicles.
 
 ## Datasources
 
 <!-- Describe each datasources you plan to use in a section. Use the prefic "DatasourceX" where X is the id of the datasource. -->
 
-### Datasource1: DB Haltetestellen
-* Metadata URL: https://data.deutschebahn.com/dataset/data-haltestellen.html
-* Data URL: https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV
-* Data Type: CSV
-
-This data set contains all important information about the stops of the German Railways (Deutsche Bahn) in Germany.
-
-### Datasource2: Kraftfahrzeugbestand
+### Datasource1: Kraftfahrzeugbestand
 * Metadata URL: https://mobilithek.info/offers/-8395690728355365851
 * Data URL: https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0020_00.csv
 * Data Type: CSV
 
-This dataset contains data on how many motor vehicles are registered in the respective counties of Germany.
+This dataset contains data on how many motor vehicles are registered in the respective counties of Germany. They are devided into different categories, such as for example Cars and Tractors.
 
-### Datasource3: Einwohner (Landkreis)
+### Datasource2: Einwohner (Landkreis)
 * Metadata URL: hhttps://www.suche-postleitzahl.org/downloads#google_vignette
 * Data URL: https://downloads.suche-postleitzahl.org/v2/public/plz_einwohner.csv
 * Data Type: CSV
 
-This dataset contains data on the different counties in Germany, such as the count of the inhabitants.
+This dataset contains data on the different counties in Germany. In the context of this project the count of the inhabitants of the respective county is of interest.
 
 ## Work Packages
 
 <!-- List of work packages ordered sequentially, each pointing to an issue with more details. -->
 
-1. Set Datapipeline
-2. Analyze data
-3. More to come
+1. Search for datasets
+2. Retrieve the data into a SQL-File
+3. Browse the data to get an overview and make a plan how to combine the tables
+4. Edit the datasets to match each other for the JOIN
+5. Plot the relevant data
+6. Insert some interesting text in the plot
 
-[i1]: https://github.com/jvalue/2023-amse-template/issues/1
+[i1]: https://github.com/Guenni-Kologe/2023-amse-template_LSc
